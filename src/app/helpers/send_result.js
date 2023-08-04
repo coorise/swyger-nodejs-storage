9448d2c428a1a810cae69f16a6c48a40b4bc5502
coorise <team.coorise@gmail.com>
2023-08-03 19:49:16 -0700

    Updating files

--- src/app/helpers/send_result.js
+++ src/app/helpers/send_result.js
@@ -96,6 +96,7 @@ let sendResult=async (service,option,req,res,next)=>{
                     .then(result => {
                         //console.log(`Signed out!`);
                     }).catch(e=>{})
+                req.extrasPath=null
 
                 if(result?.error){
                     response.error={}
@@ -230,6 +231,7 @@ let sendResultWithError=async (service,option,req,res,next)=>{
                     .then(result => {
                         //console.log(`Signed out!`);
                     }).catch(e=>{})
+                req.extrasPath=null
 
                 let error
                 if(result?.error) error={storage:result?.error}
